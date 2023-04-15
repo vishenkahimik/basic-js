b6b3cca61a367a96b72a782d371da2e11289a9f9
vishenkahimik <anastasyabormotova@gmail.com>
2023-04-15 14:50:33 +0300

    feat: add dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  } else {
+    return changeArray(members);
+  }
 }
-
+function changeArray(array) {
+  let newArray = array.filter(item => {
+    if (typeof item == 'string') {
+      return item;
+    }
+  });
+  let trimArray = [];
+  newArray.forEach(element => {
+    trimArray.push(element.trim());
+    return trimArray;
+  });
+  let changeArr = trimArray.map((item) => {
+    return item[0].toUpperCase();
+  });
+  let sortArr = changeArr.sort();
+  console.log(sortArr);
+  return sortArr.join('');
+}
+console.log(createDreamTeam([
+  '   William Alston ',
+  ' Paul Benacerraf',
+  '  Ross Cameron',
+  '       Gilles Deleuze',
+  '  Arda Denkel ',
+  '  Michael Devitt',
+  '  Kit Fine',
+  ' Nelson Goodman',
+  'David Kolb',
+  '   Saul Kripke',
+  '  Trenton Merricks',
+  '  Jay Rosenberg',
+]));
 module.exports = {
   createDreamTeam
 };

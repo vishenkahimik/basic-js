7f63efc47a46dcbc5b39ece173176bff3b8c1f7d
vishenkahimik <anastasyabormotova@gmail.com>
2023-04-16 17:50:07 +0300

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+  let commands = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  let newArr = [...arr];
+  newArr.forEach((item, index, arr) => {
+    if (item == commands[0] && arr[index + 1]) {
+      arr.splice(index + 1, 1);
+    } else if (item == commands[1] && index !== 0) {
+      arr.splice(index - 1, 1);
+    } else if (item == commands[2] && arr[index + 1]) {
+      arr[index] = arr[index + 1];
+    } else if (item == commands[3] && index !== 0 && typeof arr[index - 1] == 'number') {
+      arr[index] = arr[index - 1];
+    }
+  });
+  let filterArr = newArr.filter(item => !commands.includes(item));
+  return filterArr;
+
 }
 
 module.exports = {
